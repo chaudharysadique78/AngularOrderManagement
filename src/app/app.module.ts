4646e99fb43e55da6d2b898c98aa60a6e3165c43
chaudharysadique78 <chaudharysadique78@gmail.com>
2022-06-20 12:19:16 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { OrderformComponent } from './components/orderform/orderform.component';
+import { ThankuComponent } from './components/thanku/thanku.component';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    OrderformComponent,
+    ThankuComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
